7f24f260170a2654b23a7062e54eafd52d7fc1d9
andrebispo5 <andrefsbispo@hotmail.com>
2013-06-08 19:22:19 +0200

    Update Problema 0.rb
    added some comments

--- FaceChallenge/Problema 0.rb
+++ FaceChallenge/Problema 0.rb
@@ -1,24 +1,33 @@
+####################################################################
+#[Problema 0]                                                      #
+#If we list all the natural numbers below 10 that are multiples of #
+#3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.   #
+#                                                                  #
+#Find the sum of all the multiples of 3 or 5 below 1000.           #
+####################################################################
+
 class MultiOf
     def initialize()
-        @a=0
-        @b=0
+        @num1=0
+        @num2=0
         @max=0
         @sum=0
     end
     def InputScan
         puts "Hi, Please enter two numbers to find the sum of their multiples"
         puts "Ok, lets begin! Enter the first number."
-        @a = gets
+        @num1 = gets
         puts "Right! now another one."
-        @b = gets
+        @num2 = gets
         puts "Everything seems fine! Now enter the maximum value."
         @max = gets
     end
     def calculate
-        (1..(Integer(@max))-1).each do |i|
-            if i % Integer(@a) == 0
+        limit = Integer(@max))-1
+        (1..limit).each do |i|
+            if i % Integer(@num1) == 0
                 @sum = @sum + i
-            elsif i % Integer(@b) == 0
+            elsif i % Integer(@num2) == 0
                 @sum = @sum + i
             end
         end
@@ -34,11 +43,17 @@ class MultiOf
     end
 end
 
+
+
+#Instanciate a MultiOf class
 multi = MultiOf.new()
-#Para inserir manualmente o valor a calcular
+
+#Manual value insertion
 #multi.manualSet(3,5,1000000000)
-#Para pedir os valor que se quer na linha de comandos
+
+#Values asked from promt
 multi.InputScan
+
 multi.calculate
 multi.respond
 
